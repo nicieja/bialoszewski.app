777ae994ca709bd2ae6c965284eef0a027f4dd65
Kamil <verl@gazeta.pl>
2014-07-20 22:50:33 +0200

    AVFoundation setup

--- bialoszewski/RecordViewController.swift
+++ bialoszewski/RecordViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RecordViewController.swift
 //  bialoszewski
 //
 //  Created by Kamil Nicieja on 20/07/14.
@@ -10,17 +10,40 @@ import UIKit
 import AVFoundation
 
 class RecordViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
-                            
+
+    @IBOutlet var recordButton: UIButton
+    @IBOutlet var playButton: UIButton
+    @IBOutlet var saveButton: UIButton
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        playButton.enabled = false
+        saveButton.enabled = false
+        
+        var emitError: NSError? = nil
+        
+        let paths: NSArray = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        var pathComponents = [paths.lastObject, "file.m4a"]
+        var outputFileURL: NSURL = NSURL.fileURLWithPathComponents(pathComponents)
+        
+        let session: AVAudioSession = AVAudioSession.sharedInstance()
+        session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
+        
+        let recordSetting: NSMutableDictionary = NSMutableDictionary()
+        
+        recordSetting.setValue(kAudioFormatMPEG4AAC, forKey: AVFormatIDKey)
+        recordSetting.setValue(44100.0, forKey:AVSampleRateKey)
+        recordSetting.setValue(2, forKey:AVNumberOfChannelsKey)
+        
+        let recorder: AVAudioRecorder = AVAudioRecorder(URL: outputFileURL, settings: recordSetting, error: nil)
+        recorder.delegate = self;
+        recorder.meteringEnabled = true;
+        recorder.prepareToRecord()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
-
