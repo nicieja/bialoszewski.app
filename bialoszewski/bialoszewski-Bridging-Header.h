7086792e7c1cb83d3c2ab9beda4872ec8053a8a3
Kamil <verl@gazeta.pl>
2014-07-31 23:03:39 +0200

    Revert "DropboxSDK setup"
    This reverts commit 101bdbd3061f09203a4ea9e43866873a7f968e90.

--- bialoszewski/bialoszewski-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <Foundation/Foundation.h>
-#import <CoreGraphics/CoreGraphics.h>
-#import <Security/Security.h>
-#import <UIKit/UIKit.h>
-#import <DropboxSDK/DropboxSDK.h>
